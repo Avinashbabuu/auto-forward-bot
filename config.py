45f4dda389b279cbcc548eb3e3d546062c61cc8b
Avinashbabuu <158088631+Avinashbabuu@users.noreply.github.com>
2025-03-04 19:59:37 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-TOKEN = "8059875822:AAEpDi6yajkg9mUsOTK2ZiwRg5zBnCvANiA"
-MONGO_URI = "mongodb+srv://avinashop62:avinashop62@cluster0.5y3t1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # VPS ke liye
-DB_NAME = "avinashop62"
-ADMIN_ID = "6484788124"
